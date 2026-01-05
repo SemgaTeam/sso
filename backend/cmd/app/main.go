3b974df0e05a9c179bece1d10ecb4039c31efa57
dmxmss <dexammt@gmail.com>
2026-01-05 19:19:56 +0300

    db migrations

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"github.com/SemgaTeam/sso/internal/db"
+	"github.com/SemgaTeam/sso/internal/config"
+
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	conf := config.GetConfig()
+	migrationPath := "migrations"
+	if err := db.RunMigrations(conf.Postgres, migrationPath); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Hello world")
 }
---
+++ backend/internal/db/db.go
@@ -0,0 +1,52 @@
+package db
+
+import (
+	"github.com/SemgaTeam/sso/internal/config"
+	"gorm.io/gorm"
+	"gorm.io/driver/postgres"
+	"github.com/pressly/goose/v3"
+	_ "github.com/jackc/pgx/v5/stdlib"
+
+	"database/sql"
+	"fmt"
+)
+
+func NewPostgresConnection(conf *config.Config) (*gorm.DB, error) {
+	dsn := postgresDSN(conf.Postgres)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{TranslateError: true})	
+	if err != nil {
+		return nil, err
+	}
+
+	if conf.App.Debug {
+		db = db.Debug()
+	}
+
+	return db, nil
+}
+
+func RunMigrations(conf *config.Postgres, migrationsPath string) error {
+	dsn := postgresDSN(conf)
+	sqlDb, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return err
+	}
+	defer sqlDb.Close()
+
+	if err := goose.Up(sqlDb, migrationsPath); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func postgresDSN(conf *config.Postgres) string {
+	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		conf.Host,
+		conf.User,
+		conf.Password, 
+		conf.Db, 
+		conf.Port,
+		conf.SSLMode,
+	)
+}

ac919f563e7909600c9bed29c17d9d38f867f2cc
dmxmss <dexammt@gmail.com>
2026-02-02 18:36:52 +0300

    register use case by email test with existing user without oauth identity

--- backend/test/register_test.go
+++ backend/test/register_test.go
@@ -39,6 +39,59 @@ func TestRegisterByEmailSuccess(t *testing.T) {
 	require.Len(t, userRepo.credentials, 1)
 }
 
+func TestRegisterOauthUserExistsWithoutIdentity(t *testing.T) {
+	userRepo := &FakeUserRepository{
+		users: []core.User{
+			{
+				ID: "user_id",
+				Name: "user",
+				Email: "user@example.com",
+				Status: "active",
+			},
+		},
+	}
+
+	tokenRepo := infrastructure.NewTokenInterface("secret", jwt.SigningMethodHS256)
+	hashRepo := &FakeHashRepository{}
+
+	registerUC := core.NewRegisterUseCase(userRepo, tokenRepo, hashRepo)
+
+	token := map[string]string {
+		"email": "user@example.com",
+		"raw": "token",
+	}
+
+	ctx := context.Background()
+	input := core.RegisterInput{
+		Provider: "oauth",
+
+		Token: token,
+		Issuer: "test.issuer.com",
+		ExternalID: "test_issuer_user_id",
+	}
+
+	ssoSessionToken, err := registerUC.Execute(ctx, input)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, ssoSessionToken)
+
+	require.Len(t, userRepo.users, 1)
+	require.Len(t, userRepo.identities, 1)
+	require.Len(t, userRepo.credentials, 1)
+
+	user := userRepo.users[0]
+	identity := userRepo.identities[0]
+	credential := userRepo.credentials[0]
+
+	require.Equal(t, input.Issuer, identity.Issuer)
+	require.Equal(t, input.ExternalID, identity.ExternalID)
+	require.Equal(t, user.ID, identity.UserID)
+	require.Equal(t, input.Provider, identity.Type)
+
+	require.Equal(t, identity.ID, credential.IdentityID)
+	require.Equal(t, token["raw"], credential.Hash)
+}
+
 func TestRegisterOauthUserNotExistsWithEmailSuccess(t *testing.T) {
 	userRepo := &FakeUserRepository{
 		users: []core.User{},

81676280d9e92c9e0e57b6fe46862292844391d5
dmxmss <dexammt@gmail.com>
2026-02-08 18:44:34 +0300

    return JWKS in use case

--- backend/internal/core/getPublicKeysUseCase.go
+++ backend/internal/core/getPublicKeysUseCase.go
@@ -2,22 +2,37 @@ package core
 
 import (
 	"crypto/rsa"
+	"encoding/base64"
+	"math/big"
 )
 
 type GetPublicKeysUseCase struct {
 	keys IPrivateKeys
 }
 
-func (uc *GetPublicKeysUseCase) Execute() ([]rsa.PublicKey, error) {
+func (uc *GetPublicKeysUseCase) Execute() (JWKS, error) {
 	privateKeys, err := uc.keys.GetPrivateKeys()
 	if err != nil {
-		return nil, err
+		return JWKS{}, err
 	}
 
+	var jwks JWKS
 	var publicKeys []rsa.PublicKey
 	for _, key := range privateKeys {
 		publicKeys = append(publicKeys, key.Value.PublicKey)
+		nStr := base64.RawURLEncoding.EncodeToString(key.Value.N.Bytes())
+		eStr := base64.RawURLEncoding.EncodeToString(big.NewInt(int64(key.Value.E)).Bytes())
+
+		jwk := JWK{
+			Kty: "RSA",
+			Use: "sig",
+			Alg: "RS256",
+			N: nStr,
+			E: eStr,
+		}
+
+		jwks.Keys = append(jwks.Keys, jwk)
 	}
 
-	return publicKeys, nil
+	return jwks, nil
 }

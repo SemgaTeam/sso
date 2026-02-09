f3a3fd228ffb6401afe84d9cdaf1ef8e29eae14f
dmxmss <dexammt@gmail.com>
2026-02-09 23:17:12 +0300

    config module

---
+++ backend/internal/config/config.go
@@ -0,0 +1,70 @@
+package config
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+
+	"errors"
+	"strconv"
+	"os"
+)
+
+type Config struct {
+	PostgresURL string
+	MigrationsPath string
+	SigningKey string
+	SigningMethod jwt.SigningMethod
+	AccessTokenExp int
+	RefreshTokenExp int
+	HashCost int
+}
+
+func GetConfig() (*Config, error) {
+	dsn := os.Getenv("POSTGRES_URL")
+	if dsn == "" {
+		return nil, errors.New("POSTGRES_URL is not set")
+	}
+
+	migrationsPath := os.Getenv("MIGRATIONS_PATH")
+	if migrationsPath == "" {
+		migrationsPath = "migrations"
+	}
+
+	signingKey := os.Getenv("SIGNING_KEY")
+	if signingKey == "" {
+		return nil, errors.New("SIGNING_KEY is not set")
+	}
+
+	signingMethod := jwt.SigningMethodHS256
+
+	accessExpirationStr := os.Getenv("ACCESS_TOKEN_EXPIRATION")
+	if accessExpirationStr == "" {
+		return nil, errors.New("ACCESS_TOKEN_EXPIRATION is not set")
+	}
+	accessTokenExpiration, err := strconv.Atoi(accessExpirationStr)
+	if err != nil {
+		return nil, err
+	}
+
+	refreshExpirationStr := os.Getenv("REFRESH_TOKEN_EXPIRATION")
+	if refreshExpirationStr == "" {
+		return nil, errors.New("REFRESH_TOKEN_EXPIRATION is not set")
+	}
+	refreshTokenExpiration, err := strconv.Atoi(refreshExpirationStr)
+	if err != nil {
+		return nil, err
+	}
+
+	hashCost := 10
+
+	conf := Config{
+		PostgresURL: dsn,
+		MigrationsPath: migrationsPath,
+		SigningKey: signingKey, 
+		SigningMethod: signingMethod,
+		AccessTokenExp: accessTokenExpiration, 
+		RefreshTokenExp: refreshTokenExpiration,
+		HashCost: hashCost,
+	}
+
+	return &conf, nil
+}
--- backend/main.go
+++ backend/main.go
@@ -2,71 +2,38 @@ package main
 
 import (
 	"sso/internal/core"
+	"sso/internal/config"
 	"sso/internal/infrastructure"
 	"sso/internal/infrastructure/http"
-	"github.com/golang-jwt/jwt/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/labstack/echo/v4"
 	"github.com/pressly/goose/v3"
 
 	"context"
-	"strconv"
 	"database/sql"
 	"log"
-	"os"
 )
 
 func main() {
-	dsn := os.Getenv("POSTGRES_URL")
-	if dsn == "" {
-		log.Fatal("POSTGRES_URL is not set")
-	}
-
-	migrationsPath := os.Getenv("MIGRATIONS_PATH")
-	if migrationsPath == "" {
-		migrationsPath = "migrations"
-	}
-
-	signingKey := os.Getenv("SIGNING_KEY")
-	if signingKey == "" {
-		log.Fatal("SIGNING_KEY is not set")
-	}
-	signingMethod := jwt.SigningMethodHS256
-
-	accessExpirationStr := os.Getenv("ACCESS_TOKEN_EXPIRATION")
-	if accessExpirationStr == "" {
-		log.Fatal("ACCESS_TOKEN_EXPIRATION is not set")
-	}
-	accessTokenExpiration, err := strconv.Atoi(accessExpirationStr)
+	conf, err := config.GetConfig()
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	refreshExpirationStr := os.Getenv("REFRESH_TOKEN_EXPIRATION")
-	if refreshExpirationStr == "" {
-		log.Fatal("REFRESH_TOKEN_EXPIRATION is not set")
-	}
-	refreshTokenExpiration, err := strconv.Atoi(refreshExpirationStr)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	hashCost := 10
 
 	ctx := context.Background()
 
-	sqlDb, err := sql.Open("pgx", dsn)
+	sqlDb, err := sql.Open("pgx", conf.PostgresURL)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if err := goose.Up(sqlDb, migrationsPath); err != nil {
+	if err := goose.Up(sqlDb, conf.MigrationsPath); err != nil {
 		log.Fatal(err)
 	}
 	sqlDb.Close()
 
-	pool, err := pgxpool.New(ctx, dsn)
+	pool, err := pgxpool.New(ctx, conf.PostgresURL)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -79,9 +46,9 @@ func main() {
 	log.Println("Connected to postgres")
 	
 	clientInterface := infrastructure.NewClientInterface(pool)
-	tokenInterface := infrastructure.NewTokenInterface(signingKey, signingMethod)
+	tokenInterface := infrastructure.NewTokenInterface(conf.SigningKey, conf.SigningMethod)
 	userInterface := infrastructure.NewUserInterface(pool)
-	hashInterface := infrastructure.NewHashInterface(hashCost)
+	hashInterface := infrastructure.NewHashInterface(conf.HashCost)
 	keysInterface := infrastructure.NewKeyInterface()
 
 	privateKey, err := keysInterface.Generate("test_key")
@@ -90,7 +57,7 @@ func main() {
 	}
 	keysInterface.SavePrivateKey(privateKey)
 
-	oauthWorkflow := core.NewOAuthWorkflow(clientInterface, tokenInterface, keysInterface, accessTokenExpiration, refreshTokenExpiration)
+	oauthWorkflow := core.NewOAuthWorkflow(clientInterface, tokenInterface, keysInterface, conf.AccessTokenExp, conf.RefreshTokenExp)
 
 	loginUC := core.NewLoginUseCase(userInterface, tokenInterface, hashInterface)
 	registerUC := core.NewRegisterUseCase(userInterface, tokenInterface, hashInterface)

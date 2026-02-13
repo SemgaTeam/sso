ca679d96c30d84531646afe7d0575442ea4be7c6
dmxmss <dexammt@gmail.com>
2026-02-13 19:02:10 +0300

    [REFACTOR] move handlers to functions

--- backend/internal/infrastructure/http/server.go
+++ backend/internal/infrastructure/http/server.go
@@ -4,7 +4,6 @@ import (
 	"sso/internal/core"
 	"sso/internal/config"
 	e "sso/internal/core/errors"
-	"github.com/golang-jwt/jwt/v5"
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -12,7 +11,6 @@ import (
 
 	"context"
 	"errors"
-	"net/http"
 	"fmt"
 )
 
@@ -27,157 +25,11 @@ func SetupHandlers(conf *config.Config, e *echo.Echo, baseLogger *zap.Logger, us
 	initMiddleware(e, baseLogger)
 
 	auth := e.Group("/auth")
+	auth.POST("/login", loginHandler(loginUC))
+	auth.POST("/register", registerHandler(registerUC))
+	auth.POST("/token", oauthHandler(oauthWorkflow), tokenMiddleware)
 
-	auth.POST("/login", func(c echo.Context) error {
-		ctx := c.Request().Context()
-		params := c.QueryParams()
-
-		if params["provider"] == nil {
-			return errors.New("provider query parameter is not specified")
-		}
-		if len(params["provider"]) > 1 {
-			return errors.New("provider query parameter must be of length 1")
-		}
-
-		provider := params["provider"][0]
-		var input core.LoginInput
-
-		switch provider {
-		case "email":
-			request := map[string]string{
-				"email": "",
-				"password": "",
-			}
-			if err := c.Bind(&request); err != nil {
-				return err
-			}
-			
-			input = core.LoginInput{
-				Provider: "email",
-				Email: request["email"],
-				Password: request["password"],
-			}
-		case "oauth":
-			idToken := c.Get("id_token").(map[string]string)
-
-			input = core.LoginInput{
-				Provider: "oauth",
-				
-				Issuer: idToken["issuer"],
-				ExternalID: idToken["sub"],
-				Token: idToken,
-			}
-		}
-
-		token, err := loginUC.Execute(ctx, input)
-		if err != nil {
-			return err
-		}
-
-		return c.JSON(http.StatusOK, map[string]any{
-			"sso_session_token": token,
-		})
-	})
-
-	auth.POST("/register", func(c echo.Context) error {
-		ctx := c.Request().Context()
-		params := c.QueryParams()
-
-		if params["provider"] == nil {
-			return errors.New("provider query parameter is not specified")
-		}
-		if len(params["provider"]) > 1 {
-			return errors.New("provider query parameter must be of length 1")
-		}
-
-		provider := params["provider"][0]
-		var input core.RegisterInput
-
-		switch provider {
-		case "email":
-			request := map[string]string{
-				"name": "",
-				"email": "",
-				"password": "",
-			}
-
-			if err := c.Bind(&request); err != nil {
-				return err
-			}
-
-			input = core.RegisterInput{
-				Provider: "email",
-				Name: request["name"],
-				Email: request["email"],
-				Password: request["password"],
-			}
-
-		case "oauth":
-			idToken := c.Get("id_token").(map[string]string)
-
-			input = core.RegisterInput{
-				Provider: "oauth",
-				
-				Issuer: idToken["issuer"],
-				ExternalID: idToken["sub"],
-				Token: idToken,
-			}
-		}
-
-		token, err := registerUC.Execute(ctx, input)
-		if err != nil {
-			return err
-		}
-
-		return c.JSON(http.StatusOK, map[string]any{
-			"sso_session_token": token,
-		})
-	})
-
-	auth.POST("/token", func(c echo.Context) error {
-		ctx := c.Request().Context()
-
-		request := map[string]string{
-			"client_id": "",
-			"redirect_uri": "",
-		}
-
-		if err := c.Bind(&request); err != nil {
-			return err
-		}
-
-		token, ok := c.Get("sso_session_token").(*jwt.Token)
-		if !ok {
-			return c.JSON(http.StatusUnauthorized, map[string]any{
-				"error": "unauthorized",
-			})
-		}
-
-		userID, err := token.Claims.GetSubject()
-		if err != nil {
-			return err
-		}
-
-		accessToken, refreshToken, err := oauthWorkflow.Execute(ctx, userID, request["client_id"], request["redirect_uri"])
-		if err != nil {
-			return err
-		}
-
-		return c.JSON(http.StatusOK, map[string]any{
-			"access_token": accessToken,
-			"refresh_token": refreshToken,
-		})
-
-	}, tokenMiddleware)
-
-	e.GET("/.well-known/jwks.json", func(c echo.Context) error {
-		keys, err := jwksUC.Execute()
-		if err != nil {
-			return err
-		}
-
-		return c.JSON(http.StatusOK, keys)
-	})
+	e.GET("/.well-known/jwks.json", jwksHandler(jwksUC))
 }
 
 func errorHandler(err error, c echo.Context) {
